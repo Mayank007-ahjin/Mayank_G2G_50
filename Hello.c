c3d513a19b9ec48b61fa21c9725eb30ecbd632cf
MAYANK KARNATAK <mkarnatak23@gmail.com>
2026-02-22 15:15:04 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    printf("Hello World");
+    return 0;
+}
+//sum two numbers:
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter numbers to be added:");
+    scanf("%d%d",&a,&b);
+    printf("Thhe sum is:%d",a+b);
+    return 0;
+}
